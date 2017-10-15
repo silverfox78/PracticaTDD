921e09b6f16fce8b0594793ae372b8f08eabd9bb
Samuel Barrera <sbarrera@gmail.com>
2017-10-15 01:31:39 -0300

    actualizacion ejercicio 001

---
+++ Practica_001/src/main/java/Principal.java
@@ -0,0 +1,8 @@
+import practica001.Numero;
+
+public class Principal {
+    public static void main(String[] args) {
+        Numero numero = new Numero(5);
+        System.out.println("El numero es: " + numero.DeterminaValor());
+    }
+}
