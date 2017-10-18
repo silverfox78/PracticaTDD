a4e287f96fae1630aafd4ae81c5ed21a1c48f08f
Samuel Barrera <sbarrera@gmail.com>
2017-10-18 20:24:32 -0300

    Actualizacion

---
+++ src/main/java/practica001/MasaMuscular.java
@@ -0,0 +1,11 @@
+package practica001;
+
+public class MasaMuscular {
+    private float metros;
+    private float kilos;
+
+    public MasaMuscular(float kilos, float metros) {
+        this.kilos = kilos;
+        this.metros = metros;
+    }
+}
